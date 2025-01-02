dd57dcf08c3067049e4c71416d7e60a443089156
pl1t <mikiah.atwal@outlook.com>
2025-01-01 19:12:51 -0800

    Update main.py
    just fixed stuff yes

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import requests
-webhook_url = 'https://discordapp.com/api/webhooks/1302025516668157972/HyRYt1WwX8TNIO0CICkVIJDy9vIxu0JIuK22scHWXmeVyDkohc__lnCg5gA8LNPD-1UO'
+webhook_url = 'your webhook thingy'
 while True:
     a = {"content": "@everyone discord.gg/wxMmA57Wfv"}
     response = requests.post(webhook_url, json=a)
